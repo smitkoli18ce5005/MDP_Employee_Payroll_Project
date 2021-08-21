c2fcafe72eea24cbecf10a2471bf53ce542bd508
Smit Koli <smit.koli@morningstar.com>
2021-08-21 10:05:23 +0530

    Ability to create an Employee Payroll System Home Page UI - Note that the Header is exactly same as the Add Employee Payroll form UI. So take it as is.

---
+++ js/index.js
@@ -0,0 +1,18 @@
+class EmployeePayroll{
+    constructor(){};
+
+    getName() {
+        const name = document.getElementById('name');
+    };
+
+    getStartDate(){
+        const day = document.getElementById('day');
+        const month = document.getElementById('month');
+        const year = document.getElementById('year');
+    }
+}
+
+function validate(){
+    const obj = new EmployeePayroll();
+    obj.getName();
+}
