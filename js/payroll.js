256cfb58c3eef4d1c2c547629ce83c64695b6953
Smit Koli <smit.koli@morningstar.com>
2021-08-23 14:14:23 +0530

    Ability to view Employee Payroll details in a Tabular Format from JSON Object

--- js/payroll.js
+++ js/payroll.js
@@ -1,4 +1,6 @@
 let _name,_day,_month,_year,_profileID,_gender,_department,_salary,_notes;
+let payrollOjectList = [];
+
 
 class EmployeePayroll{
     get name(){return _name;}
@@ -14,29 +16,43 @@ class EmployeePayroll{
             const div = name.parentElement;
             div.className = 'inputs-div-name error';
             const small = div.querySelector('small');
-            small.innerText = "Name should start with captial and have atleast 3 characters"
-            alert("Name should start with captial and have atleast 3 characters!");
+            small.innerText = "Name should start with captial and have at least 3 characters"
+            alert("Name should start with captial and have at least 3 characters!");
         }
     }
 
     get profileID(){return _profileID;}
-    set profileID(profileID){
-        _profileID = profileID;
+    set profileID(profileRadio){
+        for(let i=0;i<profileRadio.length;i++){
+            if(profileRadio[i].checked){
+                _profileID = profileRadio[i].value;
+            }
+        }
     }
 
     get gender(){return _gender;}
     set gender(gender){
-        _gender = gender;
+        for(let i=0;i<gender.length;i++){
+            if(gender[i].checked){
+                _gender = gender[i].value;
+            }
+        }
     }
 
     get department(){return _department;}
     set department(department){
-        _department = department;
+        let departmentList = [];
+        for(let i=0;i<department.length;i++){
+            if(department[i].checked){
+                departmentList.push(department[i].value);
+            }
+        }
+        _department = departmentList;
     }
 
     get salary(){return _salary;}
     set salary(salary){
-        _salary = salary;
+        _salary = salary.value;
     }
 
     get date(){return [_day,_month,_year];}
@@ -50,9 +66,9 @@ class EmployeePayroll{
             div.className = 'inputs inputs-450px';
             const small = div.querySelector('small');
             small.innerText = ""
-            _day = day;
-            _month = month;
-            _year = year;
+            _day = day.value;
+            _month = month.value;
+            _year = year.value;
             console.log("valid date");
         }
         else{
@@ -67,15 +83,112 @@ class EmployeePayroll{
     
     get notes(){return _notes;}
     set notes(notes){
-        _notes = notes;
+        _notes = notes.value;
     }
 }
 
+const emp = new EmployeePayroll();
 
 function validate(){
-    const emp = new EmployeePayroll();
     emp.name = document.getElementById('name');
-    console.log(emp.name);
+    emp.profileID = document.getElementsByName('profile-radio');
+    emp.gender = document.getElementsByName('gender');
+    emp.department = document.getElementsByName('department');
+    emp.salary = document.getElementById('salary');
     emp.date = [document.getElementById('day'), document.getElementById('month'), document.getElementById('year')];
-    console.log(emp.date);
+    emp.notes = document.getElementById('notes');
+    createPayrollObject();
 }
+
+function createPayrollObject() {
+    let payrollOject = {};
+    if(_name != undefined && _day != undefined && _month != undefined && _year != undefined){
+        payrollOject["name"] = emp.name;
+        payrollOject["profileID"] = emp.profileID;
+        payrollOject["gender"] = emp.gender;
+        payrollOject["department"] = emp.department;
+        payrollOject["salary"] = emp.salary;
+        payrollOject["date"] = emp.date;
+        payrollOject["notes"] = emp.notes;
+        payrollOjectList.push(payrollOject)
+        console.log(payrollOject);
+        console.log(payrollOjectList);
+    }
+}
+
+function reset() {
+    document.getElementById('name').value = '';
+    document.getElementById('profile-photo-1').checked = false;
+    document.getElementById('profile-photo-2').checked = false;
+    document.getElementById('profile-photo-3').checked = false;
+    document.getElementById('profile-photo-4').checked = false;
+    document.getElementById('male').checked = false;
+    document.getElementById('female').checked = false;
+    document.getElementById('department-1').checked = false;
+    document.getElementById('department-2').checked = false;
+    document.getElementById('department-3').checked = false;
+    document.getElementById('department-4').checked = false;
+    document.getElementById('department-5').checked = false;
+    document.getElementById('day').value = 0;
+    document.getElementById('month').value = 0;
+    document.getElementById('year').value = 0;
+    document.getElementById('notes').value = '';
+}
+
+let empObjList = [
+    {
+        profileID: "../assets/Ellipse -2.png",
+        name: "Smit",
+        gender: "Male",
+        department: ["Sales", "HR"],
+        salary: 28900,
+        date: ["18", "8", "2021"],
+        notes: "This is me"
+    },
+    {
+        profileID: "../assets/Ellipse -2.png",
+        name: "Koli",
+        gender: "Male",
+        department: ["Sales", "HR"],
+        salary: 28900,
+        date: ["18", "8", "2021"],
+        notes: "This is me"
+    },
+    {
+        profileID: "../assets/Ellipse -2.png",
+        name: "Amit",
+        gender: "Male",
+        department: ["Sales", "HR"],
+        salary: 28900,
+        date: ["18", "8", "2021"],
+        notes: "This is me"
+    }
+]
+
+let table_header =`<tr>
+                <th></th>
+                <th>NAME</th>
+                <th>GENDER</th>
+                <th>DEPARTMENT</th>
+                <th>SALARY</th>
+                <th>START DATE</th>
+                <th>ACTIONS</th>
+                </tr>`;
+
+let table_content = `${table_header}`
+
+empObjList.forEach(empObj => {
+    table_content = `${table_content}
+    <tr>
+    <td><img src="${empObj.profileID}" alt="1"></td>
+    <td>${empObj.name}</td>
+    <td>${empObj.gender}</td>
+    <td>${empObj.department}</td>
+    <td>&#x20B9; ${empObj.salary}</td>
+    <td>${empObj.date}</td>
+    <td>&#128465; &nbsp; &#9998;</td>
+    </tr>`
+    console.log(table_content);
+
+    document.getElementById('payroll-table').innerHTML = table_content;
+});
--- js/validate.js
+++
@@ -1,3 +0,0 @@
-import {  } from '../js/payroll.js';
-
-
