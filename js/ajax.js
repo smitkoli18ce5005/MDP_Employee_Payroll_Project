eb4fcddd2de17d12abdf39ced954f782a1a71b09
Smit Koli <smit.koli@morningstar.com>
2021-08-27 22:45:32 +0530

    Display employee data using ajax-promise

--- js/ajax.js
+++ js/ajax.js
@@ -1,27 +1,35 @@
 let home_url = "http://localhost:3000/EmployeePayrollList/";
-function makeAjaxCall(methodType, url, callback, async = false, data = null) {
-  const xhttp = new XMLHttpRequest();
+function makeAjaxCall(methodType, url, async = false, data = null) {
+  return new Promise (function (resolve, reject) {
+    const xhttp = new XMLHttpRequest();
 
-  xhttp.onreadystatechange = function () {
-    console.log("ReadyState: " + xhttp.readyState);
-    if (xhttp.readyState == 4) {
-      console.log("Started!");
-      if (xhttp.status == 200) {
-        callback(xhttp.responseText);
-      } else {
-        console.error("Server Failed!");
+    xhttp.onreadystatechange = function () {
+      console.log("ReadyState: " + xhttp.readyState);
+      if (xhttp.readyState == 4) {
+        console.log("Started!");
+        if (xhttp.status == 200) {
+          resolve(xhttp.responseText);
+        } else if (xhttp.status >= 400) {
+          reject(error)
+          console.error("Server Failed!");
+        }
       }
+    };
+  
+    xhttp.open(methodType, url, async);
+    if (data) {
+      xhttp.setRequestHeader("Content-Type", "application/json");
+      xhttp.send(JSON.stringify(data));
+    } else {
+      xhttp.send();
     }
-  };
-
-  xhttp.open(methodType, url, async);
-  xhttp.send();
+  });
 }
 
 function display(str) {
-  console.log(str);
-}
-
-function displayData() {
-  makeAjaxCall("get", home_url, display, async=true);
+  makeAjaxCall("get", home_url, async=true).then(
+    responseText => console.log("Respose from the server: " +responseText)
+  ).catch(
+    error => {console.log("Server responded with error\n" +error)}
+  )
 }
